84ed32d14a5151bfa99e359d59112ce55963632c
“HyruleTrash” <83176601+HyruleTrash@users.noreply.github.com>
2025-11-19 11:48:05 +0100

    Added: Monogame

--- Program.cs
+++ Program.cs
@@ -1,9 +1,49 @@
 ﻿namespace ProdToolDOOM;
 
-class Program
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+class Program : Game
 {
+    private GraphicsDeviceManager graphics;
+    
+    public Program()
+    {
+        graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+    
     static void Main(string[] args)
     {
-        System.Console.WriteLine("Hello World!");
+        Console.WriteLine("Starting application...");
+        var p = new Program();
+        p.Run();
+    }
+    
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+    
+    protected override void LoadContent()
+    {
+        // _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        base.Draw(gameTime);
     }
 }
